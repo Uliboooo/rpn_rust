c0fda773402dfbaa35acb506d3769107f735cdea
yuki <fu08yu20ko07@icloud.com>
2024-05-28 18:37:12 +0900

    入力された文字列の検査機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use regex::Regex;
+
 fn main() {
     loop {
-        println!("Please input formula.\nex: 1 + 2 is 1 2 +\nDelimiter is half-width space.");
+         println!("式を入力してください。\n例: 1 + 2 → 1 2 +\n値や演算子同士は半角スペースで区切ってください。");
         let input_formula = get_input();
+        if check_syntax(&input_formula) == false {
+            println!("計算不可能な文字が含まれています。もう1度入力してください");
+            continue;
+        }
         let delimited_input_fomula = delimit(&input_formula);
         let mut stack = Vec::<f64>::new();
         let mut result = 0.0;
@@ -22,7 +28,7 @@ fn main() {
                 }
             }
         }
-        println!("{}\nAgain?(y/n)", result);
+        println!("{}\nもう一度計算しますか?(y/n)", result);
         if get_input() == "n".to_string() {
             break;
         }
@@ -35,6 +41,11 @@ fn get_input() -> String { //String型で入力を返す
         .read_line(&mut word)
         .expect("Failed to read line");
     word.trim().to_string()
+}
+
+fn check_syntax(checked_string: &String) -> bool { //入力に演算不可能な文字があった場合false
+    let re = Regex::new("[^+\\-*/%1234567890 ]").unwrap();
+    return if re.is_match(&checked_string) {false} else {true};
 }
 
 fn delimit(input: &String) -> Vec<&str>{ //文字列を空白で区切りベクタにして返す
@@ -60,7 +71,7 @@ fn calculation(operand_1: f64, operand_2: f64, operator: &str) -> f64 { //演算
     }
 }
 
-fn power(operand_1: f64, operand_2: f64) -> f64 {
+fn power(operand_1: f64, operand_2: f64) -> f64 { //指数演算
     let mut power_result = operand_1;
     for _ in 0..operand_2 as i64 - 1 {
         power_result *= operand_1
