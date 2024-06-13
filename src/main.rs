89ecbdf8c1709800acec5575196c662bb017fe5a
yuki <fu08yu20ko07@icloud.com>
2024-06-13 18:41:03 +0900

    入力されなかった場合の処理を追加

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,33 @@ fn get_input() -> String { //String型で入力を返す
     word.trim().to_string()
 }
 
-fn check_syntax(checked_string: &String) -> bool { //入力に演算不可能な文字があった場合false
+fn check_length(checked_string: &String) -> bool{
+    if checked_string.len() == 0 {
+        println!("式が入力されていない可能性があります。もう一度入力してください。\n");
+        false
+    } else {
+        true
+    }
+}
+
+fn check_unavailable_character(checked_string: &String) -> bool {
     let re = Regex::new("[^+\\-*/%1234567890 ]").unwrap();
-    return if re.is_match(&checked_string) {false} else {true};
+    if re.is_match(&checked_string) {
+        println!("計算不可能な文字が含まれています。もう1度入力してください\n");
+        false
+    } else {
+        true
+    }
+}
+
+fn check_syntax(checked_string: &String) -> bool { //入力に演算不可能な文字があった場合false
+    if check_unavailable_character(checked_string) == false {
+        false
+    } else if check_length(checked_string) == false {
+        false
+    } else {
+        true
+    }
 }
 
 fn delimit(input: &String) -> Vec<&str>{ //文字列を空白で区切りベクタにして返す
@@ -46,7 +70,8 @@ fn power(operand_1: f64, operand_2: f64) -> f64 { //指数演算
 
 fn stack_manage(delimited_input: Vec<&str>) -> f64{
     let mut stack = Vec::<f64>::new();
-    let mut result = 0.0;
+    // let mut result = delimited_input[0];
+    let mut result = if is_numeric(delimited_input[0]) == true {delimited_input[0].parse::<f64>().unwrap_or(0.0)} else {0.0};
     for i in delimited_input {
         if is_numeric(i) == true { //オペランドの場合
             stack.push(i.parse::<f64>().unwrap_or(0.0));
@@ -66,10 +91,7 @@ fn main() {
         println!("rpn_rust\nver.1.1");
         println!("式を入力してください。\n例: 1 + 2 → 1 2 +\n値や演算子同士は半角スペースで区切ってください。");
         let input_formula = get_input();
-        if check_syntax(&input_formula) == false {
-            println!("計算不可能な文字が含まれています。もう1度入力してください");
-            continue;
-        }
+        if check_syntax(&input_formula) == false {continue;}
         let delimited_input_fomula = delimit(&input_formula);
         let result = stack_manage(delimited_input_fomula);
         println!("{}\nもう一度計算しますか?(y/n)", result);
